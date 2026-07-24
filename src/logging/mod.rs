5421a0bb3c2f6993399ccb014cbee8268315f210
Ewan Croft <git@ewancroft.uk>
2026-07-24 23:10:50 +0100

    feat: add structured logging configuration
    - Implement init_logger() with tracing-subscriber
    - Support env-filter for log level control
    - Add JSON and pretty-print formats

---
+++ src/logging/mod.rs
@@ -0,0 +1,23 @@
+use tracing_subscriber::{EnvFilter, fmt};
+use tracing_subscriber::prelude::*;
+
+pub fn init_logger(log_level: Option<String>, test_mode: bool) -> anyhow::Result<()> {
+    let level = log_level.unwrap_or_else(|| "info".to_string());
+    let env_filter = EnvFilter::try_new(&level)?;
+
+    if test_mode {
+        tracing_subscriber::registry()
+            .with(env_filter)
+            .with(fmt::layer().pretty())
+            .try_init()
+            .ok();
+    } else {
+        tracing_subscriber::registry()
+            .with(env_filter)
+            .with(fmt::layer().json())
+            .try_init()
+            .ok();
+    }
+
+    Ok(())
+}
